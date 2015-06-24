64aa45a6ef0735611da37234d0b9f2a370bb4a8d
frohland <florianrohland@gmx.de>
2015-06-24 09:14:43 +0200

    added ezqueries as new content element to the typo3 new content wizard

--- ext_tables.php
+++ ext_tables.php
@@ -12,4 +12,5 @@ if (!defined ('TYPO3_MODE')) {
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_ezqueriesplugin'] = 'pi_flexform';
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_ezqueriesplugin', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_ezqueriesplugin.xml');
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'EasyQueries');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ezqueries/Configuration/TypoScript/pageTsConfig.txt">');
 ?>
